0874778475232f0356bcf47d28e53549fc77dbe3
Raffael Martins <rbarbosama@indracompany.com>
2021-02-12 09:59:24 -0300

    Adicionar modulo perfil com canActivate

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,4 @@
+import { AuthGuard } from './core/guards/auth.guard';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
@@ -6,6 +7,14 @@ const routes: Routes = [
     path: '',
     loadChildren: () =>
       import('./modules/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'profile',
+    /* Remove comments to activate guard
+    canActivate: [AuthGuard],
+    **/
+    loadChildren: () =>
+      import('./modules/profile/profile.module').then((m) => m.ProfileModule),
   },
   {
     path: '',
